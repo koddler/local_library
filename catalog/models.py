7e6b5aed96347da7d916311b9f2c2b1daa955308
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-09-26 17:58:45 +0600

    Add model/migration for Language

--- catalog/models.py
+++ catalog/models.py
@@ -14,6 +14,16 @@ class Genre(models.Model):
         return self.name
 
 
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
+
 class Book(models.Model):
     """Model representing a book."""
     title = models.CharField(max_length=200)
@@ -31,6 +41,8 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(
         Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey(
+        'Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """String for representing the Model object."""
