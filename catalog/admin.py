85e6b9453610aea42c62bdf8d6a652dcddf7bc58
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-09-28 16:54:13 +0600

    Add BookInstance as inlines for DetailView of BookAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -19,9 +19,15 @@ class AuthorAdmin(admin.ModelAdmin):
 admin.site.register(Author, AuthorAdmin)
 
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
