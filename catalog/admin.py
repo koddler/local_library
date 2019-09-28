2f4345d1c6632d6979f17c7a399d53a7a6423ee3
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-09-28 16:56:44 +0600

    Update fields to display in ListView of BookInstanceAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -32,7 +32,7 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
