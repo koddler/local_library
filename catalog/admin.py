8dc71e37fb3ab8aafedfc9b9430192eb49b58179
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-09-28 16:59:51 +0600

    Add Book to inlines of AuthorAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,10 +10,16 @@ admin.site.register(Genre)
 admin.site.register(Language)
 
 
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 
 admin.site.register(Author, AuthorAdmin)
