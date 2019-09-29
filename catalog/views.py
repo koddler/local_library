b61dfefd9496b778ef19a5ed447625bb8f524916
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-09-29 18:10:04 +0600

    Add ListView for Author
    * Add view AuthorList
    * Add url mapping for AuthorList
    * Add template `author_list`
    * Update link to AuthorList in `base_generic` template

--- catalog/views.py
+++ catalog/views.py
@@ -44,3 +44,8 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorList(generic.ListView):
+    model = Author
+    paginate_by = 5
