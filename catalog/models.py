6de08ae02c728cc529617a5cab024dc90daf179c
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-10-05 22:51:16 +0600

    Update model BookInstance
    * Add ForeignKey field borrower
    * Add property `is_overdue`
    * Add migrations for change

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,9 @@
 import uuid
+from datetime import date
 
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Genre(models.Model):
@@ -66,6 +68,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -88,6 +92,12 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
