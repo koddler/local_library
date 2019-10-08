fe82f8f8fa9a785fd9e2000b1712b68b9c197e99
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-10-08 14:15:35 +0600

    Update Edit views, Add/Edit templates
    * Add permissions to all Author Edit views
    * Add url mapping for Author Edit views
    * Add templates for the Edit views
    * Conditionally link to Edit views on Author list page

--- catalog/views.py
+++ catalog/views.py
@@ -123,17 +123,20 @@ def renew_book_librarian(request, pk):
     return render(request, 'catalog/book_renew_librarian.html', context)
 
 
-class AuthorCreate(CreateView):
+class AuthorCreate(PermissionRequiredMixin, CreateView):
     model = Author
     fields = '__all__'
     initial = {'date_of_death': '05/01/2018'}
+    permission_required = ('catalog.can_mark_returned', )
 
 
-class AuthorUpdate(UpdateView):
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    permission_required = ('catalog.can_mark_returned', )
 
 
-class AuthorDelete(DeleteView):
-    Model = Author
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    permission_required = ('catalog.can_mark_returned', )
     success_url = reverse_lazy('authors')
