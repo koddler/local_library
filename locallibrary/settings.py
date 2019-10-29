c825cde1f1f406ea9f85f7b6d61bce1c028d6391
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2019-10-29 17:21:48 +0600

    Update settings for production

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kz%vs(ldmg$tsu!ao!*g$fdo2!qo4k@*t8!ign_&v#bnzp4h%q'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'kz%vs(ldmg$tsu!ao!*g$fdo2!qo4k@*t8!ign_&v#bnzp4h%q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
